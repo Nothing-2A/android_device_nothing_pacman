ca45fb5182e41ed829e3f781e11adac9226c646e
Woomymy <woomy@woomy.be>
2023-03-03 11:48:06 +0100

    pacman: Inherit generic_ramdisk product
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I93e080fd483d88be3a389a0ac64542ee04298f03

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
