b9f7e671ac0189ad3f103cb9e9f62e4fee54152e
iamlordutkarsh <lalbaake456@gmail.com>
2024-12-19 06:43:41 +0000

    pacman: Define Bootloader and Platform flags
    Signed-off-by: iamlordutkarsh <lalbaake456@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,5 +14,13 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := pacman
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6886
+BOARD_HAS_MTK_HARDWARE := true
+
 # Inherit the proprietary files
 include vendor/nothing/pacman/BoardConfigVendor.mk
