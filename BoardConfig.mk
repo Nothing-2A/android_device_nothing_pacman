8bc81cf7d98aea764ab4363104db6b52a25e4c5b
bengris32 <bengris32@protonmail.ch>
2024-12-19 06:39:30 +0000

    pacman: Define Architecture flags
    Signed-off-by: iamlordutkarsh <lalbaake456@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,13 @@
 
 DEVICE_PATH := device/nothing/pacman
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
+
 # Inherit the proprietary files
 include vendor/nothing/pacman/BoardConfigVendor.mk
